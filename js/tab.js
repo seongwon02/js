cbf296948ea498bab6459c8f935375bd86381464
김성원 <seongwon2303@gmail.com>
2024-01-12 17:13:50 +0900

    24.01.12

--- js/tab.js
+++ js/tab.js
@@ -1,9 +1,23 @@
-var 버튼 = $('.tab-button');
-for (let i = 0; i < 3; i++) {
-	버튼.eq(0).on('click', function(){
-	  버튼.removeClass('orange');
-	  버튼.eq(0).addClass('orange');
-	  $('.tab-content').removeClass('show');
-	  $('.tab-content').eq(0).addClass('show');
-	})
+// for (let i = 0; i < 3; i++) {
+// 	$('.tab-button').eq(i).on('click', function(){
+// 	  openTab(i)
+// 	})
+// }
+
+
+$('.list').click(function(e){
+	openTab(e.target.dataset.id);
+})
+
+var car2 = { name : '소나타', price : 50000 } 
+
+$('#name').html(car2.name);
+$('#price').html(car2.price);
+
+
+function openTab(num){
+	$('.tab-button').removeClass('orange');
+	$('.tab-button').eq(num).addClass('orange');
+	$('.tab-content').removeClass('show');
+	$('.tab-content').eq(num).addClass('show');
 }
