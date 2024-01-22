de962a0e5302d4a3d73cfaf78dc2b991c362baa5
김성원 <seongwon2303@gmail.com>
2024-01-22 17:49:46 +0900

    24.01.22

--- js/tab.js
+++ js/tab.js
@@ -5,17 +5,37 @@
 // }
 
 
-$('.list').click(function(e){
+$('.form-select').eq(0).on('click', function () {
+	var value = $('.form-select').eq(0).val();
+	
+	if (value == '셔츠') {
+		$('.form-select').eq(1).removeClass('form-hide');
+		$('.form-select').eq(1).html('');
+		var shirt = '<option>100</option><option>105</option>';
+		$('.form-select').eq(1).append(shirt)
+	}else if(value == '바지'){
+		$('.form-select').eq(1).removeClass('form-hide');
+		$('.form-select').eq(1).html('');
+		var pants = '<option>28</option><option>30</option>';
+		$('.form-select').eq(1).append(pants)
+	}else{
+		$('.form-select').eq(1).addClass('form-hide');
+	}
+})
+
+
+
+$('.list').click(function (e) {
 	openTab(e.target.dataset.id);
 })
 
-var car2 = { name : '소나타', price : 50000 } 
+var car2 = { name: '소나타', price: 50000 }
 
 $('#name').html(car2.name);
 $('#price').html(car2.price);
 
 
-function openTab(num){
+function openTab(num) {
 	$('.tab-button').removeClass('orange');
 	$('.tab-button').eq(num).addClass('orange');
 	$('.tab-content').removeClass('show');
